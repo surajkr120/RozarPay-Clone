bb18eb9cdd5fa6775205c8bc58851fa59b28c58f
surajkr120 <144226400+surajkr120@users.noreply.github.com>
2024-09-01 10:43:26 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      greenLight: "#61cea6",
+      grayText: "#818579",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+      white: "#ffffff",
+      gray300: "#d1d5db",
+    },
+  },
+  plugins: [],
+};
